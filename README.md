b41126647cf9124097c7ab9b08c848a0223f0878
Paul Rosenfeld <dramninjas@gmail.com>
2013-04-04 11:49:29 -0400

    Delete the repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# This project has been merged into the marss repository #
+
+See the util/ folder in https://github.com/avadhpatel/marss
--- poll_gmail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-# this file is intended to be used with cron -- it kills the output 
-# so cron doesn't send you an email every time the script runs
-
-#python -W ignore::DeprecationWarning ./poll_gmail.py &> /dev/null
-export PATH=$PATH:$HOME/marss.utils 
-poll_gmail.py &> /dev/null
